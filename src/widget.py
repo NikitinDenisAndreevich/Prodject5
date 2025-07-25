980a896ee56ca8bf1749cbb6b0f74d6e94803358
Денис <_email@example.com>
2025-07-26 01:37:37 +0300

    Маскирует номер карты или счета в зависимости от типа

---
+++ src/widget.py
@@ -0,0 +1,26 @@
+import masks
+
+
+def mask_account_card(card: str) -> str:
+    """Маскирует номер карты или счета в зависимости от типа
+    """
+
+    # Ключевое слово для идентификации типа "Счет"
+    test_word = "счет"
+
+    # Разбиваем строку на составляющие слова
+    card_parts = card.split()
+
+    # Инициализируем переменную для результата
+    masked_result = ""
+
+    if test_word in card_parts:
+        # Маскировка для счета (берем последние 20 символов)
+        masked_number = masks.get_mask_card_number(card[-20:])
+        masked_result = card[:-20] + masked_number
+    else:
+        # Маскировка для карты (берем последние 16 символов)
+        masked_number = masks.get_mask_account(card[-16:])
+        masked_result = card[:-16] + masked_number
+
+    return masked_result
