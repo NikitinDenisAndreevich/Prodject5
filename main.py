6d4490ffedcce0c971a4a8121dfd6c0646a3cdb4
Денис <_email@example.com>
2025-07-26 01:39:24 +0300

    перенес main.py  в голову

---
+++ main.py
@@ -0,0 +1,5 @@
+import src\
+
+number_card = input("Ведите номер карты. 16 чисел")
+
+print(src.masks.mask_account_card(number_card))
--- src/main.py
+++
@@ -1,7 +0,0 @@
-import masks
-
-number_card = input("Ведите номер карты. 16 чисел")
-
-print(masks.get_mask_card_number(number_card))
-
-print(masks.get_mask_account(number_card))
