8083f8768076271795c8fccb72dd0f4d8ce547fa
Денис <_email@example.com>
2025-07-27 22:06:21 +0300

    27.07 незначительные корректировки кода import изменил убрал лишние строки

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from . import masks
+from src import masks
 
 
 def mask_account_card(card: str) -> str:
@@ -11,15 +11,12 @@ def mask_account_card(card: str) -> str:
     # Разбиваем строку на составляющие слова
     card_parts = card.lower().split()
 
-    # Инициализируем переменную для результата
-    masked_result = ""
-
     if test_word in card_parts:
-        # Маскировка для карты (берем последние 16 символов)
+        # Маскировка для карты (берем последние 20 символов)
         masked_number = masks.get_mask_account(card[-20:])
         masked_result = card[:-20] + masked_number
     else:
-        # Маскировка для счета (берем последние 20 символов)
+        # Маскировка для счета (берем последние 16 символов)
         masked_number = masks.get_mask_card_number(card[-16:])
         masked_result = card[:-16] + masked_number
 
